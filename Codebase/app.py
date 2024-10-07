b7fa99a92605581b3d767a48e4881492646820b5
HayrichDevOps <hayrich@ultimatebodylabs.nl>
2024-10-07 11:22:47 +0200

    added new files and renamed app.py

---
+++ Codebase/app.py
@@ -0,0 +1,10 @@
+#code
+print("Welcome to nubia-native.nl")
+print("Please fill in a number and hit enter!")
+num = int(input())
+
+if num%2 == 0:
+    print("It's an Even Number!")
+
+else:
+    print("It's an Odd number!")    
